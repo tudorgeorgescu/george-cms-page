a7d655dd61fc66b2e05f87326012c672fbd9bbb2
iBurda <H50CHWA@s-mxs.net>
2021-08-10 15:43:52 +0200

    added support for more icons

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,14 +1,36 @@
 import React from "react";
 import { AiFillCar } from "react-icons/ai";
 import { FaRegSave } from "react-icons/fa";
+import { FaCcMastercard } from "react-icons/fa";
+import { FaCreditCard } from "react-icons/fa";
+import { BsCreditCard } from "react-icons/bs";
+import { SiProducthunt } from "react-icons/si";
+import { BsGift } from "react-icons/bs";
+import { MdWallpaper } from "react-icons/md";
+import { FaMoneyBillWaveAlt } from "react-icons/fa";
+import { FaCcVisa } from "react-icons/fa";
+import { FaRegFileArchive } from "react-icons/fa";
+import { CgUnavailable } from "react-icons/cg";
+
 import Classes from "./ProductCard.module.css";
 
-let components = {
-  AiFillCar: AiFillCar,
+let icons = {
+  AiFillCar,
+  FaRegSave,
+  FaCcMastercard,
+  FaCreditCard,
+  BsCreditCard,
+  SiProducthunt,
+  BsGift,
+  MdWallpaper,
+  FaMoneyBillWaveAlt,
+  FaCcVisa,
+  FaRegFileArchive,
+  CgUnavailable
 };
 
 export default function ProductCard({ id, title, subtitle, productId, icon, backgroundColor, type }) {
-  let TagName = components[icon];
+  let TagName = icons[icon] ? icons[icon] : CgUnavailable;
 
   return (
     <div className={Classes.ProductCard}>
